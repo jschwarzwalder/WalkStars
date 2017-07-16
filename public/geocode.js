b4efcff06c9fe06d5ecda38320d1197f43d20585
Salamagundi <ajmw.subs@gmail.com>
2017-07-15 20:58:46 -0700

    added start

---
+++ public/geocode.js
@@ -0,0 +1,21 @@
+function continuousGeocode(){
+  setTimeout(
+    function(){
+     document.getElementById('loc').textContent = 'hi! ';
+      navigator.geolocation.getCurrentPosition(function(position) {
+           document.getElementById('loc').textContent = 'hi! ' + position.coords.latitude + ' ' + position.coords.longitude;
+           continuousGeocode();
+      }, function(err){
+          document.getElementById('loc').textContent = 'err: ' + err;
+          continuousGeocode();
+      });
+    }, 1000)
+}
+
+if (navigator && navigator.geolocation) {
+  /* geolocation is available */
+   continuousGeocode();
+} else {
+  /* geolocation IS NOT available */
+           document.getElementById('loc').textContent = "NOOOOOOOOOO!";
+}
