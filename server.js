1bce5daff675a47ebcedc312c7ac2f5a5d2e4b91
abraxasrex <jbreasor@gmail.com>
2017-07-15 21:21:59 -0700

    switcheroo

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, './', 'index.html'));
 });
 
-let port = 3000 || process.env.PORT;
+let port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Example app listening on port 3000!')
 })
