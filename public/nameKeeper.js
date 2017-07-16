286d0c647c64acb407f3fcc734354bcc554ac4f3
Salamagundi <ajmw.subs@gmail.com>
2017-07-16 12:17:59 -0700

    added name script

---
+++ public/nameKeeper.js
@@ -0,0 +1,9 @@
+  
+function getName(){
+getName.staticProperty = document.getElementById('player_name').value;
+}
+
+function displayName (vari){
+  document.getElementById('player_name').innerHTML = getName.staticProperty;
+}
+ 
